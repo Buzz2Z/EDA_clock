3f50d3dc2fc405b0809a8045fda5463fbb29ba31
Buzz2Z <1574610489@qq.com>
2023-12-06 19:52:42 +0800

    在顶层模块中增加了整点播报模块,稍微修改了一下管脚

--- test_clock.v
+++ test_clock.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Full Version"
-// CREATED		"Wed Dec 06 14:59:59 2023"
+// CREATED		"Wed Dec 06 19:51:58 2023"
 
 module test_clock(
 	clk,
@@ -25,7 +25,8 @@ module test_clock(
 	AdjMinKey,
 	AdjHrKey,
 	mode,
-	alarm_beep,
+	alarm,
+	radio,
 	dig,
 	seg
 );
@@ -39,23 +40,25 @@ input wire	rstn;
 input wire	AdjMinKey;
 input wire	AdjHrKey;
 input wire	mode;
-output wire	alarm_beep;
+output wire	alarm;
+output wire	radio;
 output wire	[7:0] dig;
 output wire	[7:0] seg;
 
 wire	SYNTHESIZED_WIRE_0;
-wire	SYNTHESIZED_WIRE_20;
-wire	SYNTHESIZED_WIRE_2;
-wire	SYNTHESIZED_WIRE_3;
-wire	[3:0] SYNTHESIZED_WIRE_21;
-wire	[3:0] SYNTHESIZED_WIRE_22;
-wire	[3:0] SYNTHESIZED_WIRE_23;
-wire	[3:0] SYNTHESIZED_WIRE_24;
-wire	[3:0] SYNTHESIZED_WIRE_25;
-wire	[3:0] SYNTHESIZED_WIRE_26;
+wire	SYNTHESIZED_WIRE_31;
+wire	SYNTHESIZED_WIRE_32;
+wire	[3:0] SYNTHESIZED_WIRE_33;
+wire	[3:0] SYNTHESIZED_WIRE_34;
+wire	[3:0] SYNTHESIZED_WIRE_35;
+wire	[3:0] SYNTHESIZED_WIRE_36;
 wire	SYNTHESIZED_WIRE_11;
+wire	[3:0] SYNTHESIZED_WIRE_37;
+wire	[3:0] SYNTHESIZED_WIRE_38;
 wire	[7:0] SYNTHESIZED_WIRE_18;
 wire	[7:0] SYNTHESIZED_WIRE_19;
+wire	SYNTHESIZED_WIRE_20;
+wire	SYNTHESIZED_WIRE_21;
 
 
 
@@ -64,90 +67,102 @@ wire	[7:0] SYNTHESIZED_WIRE_19;
 clock_part	b2v_inst(
 	.clk(clk),
 	.enable(SYNTHESIZED_WIRE_0),
-	.rstn(rstn),
+	.rstn(SYNTHESIZED_WIRE_31),
 	.AdjMinKey(AdjMinKey),
 	.AdjHrKey(AdjHrKey),
-	.clk_1Hz(SYNTHESIZED_WIRE_20),
-	.h_cntH(SYNTHESIZED_WIRE_21),
-	.h_cntL(SYNTHESIZED_WIRE_22),
-	.m_cntH(SYNTHESIZED_WIRE_23),
-	.m_cntL(SYNTHESIZED_WIRE_24),
-	.s_cntH(SYNTHESIZED_WIRE_25),
-	.s_cntL(SYNTHESIZED_WIRE_26));
+	.clk_1Hz(SYNTHESIZED_WIRE_32),
+	.h_cntH(SYNTHESIZED_WIRE_37),
+	.h_cntL(SYNTHESIZED_WIRE_38),
+	.m_cntH(SYNTHESIZED_WIRE_33),
+	.m_cntL(SYNTHESIZED_WIRE_34),
+	.s_cntH(SYNTHESIZED_WIRE_35),
+	.s_cntL(SYNTHESIZED_WIRE_36));
 
 
 clk_en_generator	b2v_inst1(
 	.clk(clk),
-	.rstn(rstn),
-	.enable(SYNTHESIZED_WIRE_2));
-	defparam	b2v_inst1.N = 5;
+	.rstn(SYNTHESIZED_WIRE_31),
+	.enable(SYNTHESIZED_WIRE_20));
+	defparam	b2v_inst1.N = 50000;
 	defparam	b2v_inst1.W = 16;
 
 
 clk_en_generator	b2v_inst10(
 	.clk(clk),
-	.rstn(rstn),
+	.rstn(SYNTHESIZED_WIRE_31),
 	.enable(SYNTHESIZED_WIRE_0));
-	defparam	b2v_inst10.N = 500;
+	defparam	b2v_inst10.N = 1000000;
 	defparam	b2v_inst10.W = 23;
 
 
 clk_en_generator	b2v_inst11(
 	.clk(clk),
-	.rstn(rstn),
+	.rstn(SYNTHESIZED_WIRE_31),
 	.enable(SYNTHESIZED_WIRE_11));
-	defparam	b2v_inst11.N = 5;
+	defparam	b2v_inst11.N = 50000;
 	defparam	b2v_inst11.W = 16;
 
 
-bell_part	b2v_inst2(
-	.setHrKey(setHrKey),
-	.setMinKey(setMinKey),
-	.CtrlBell(CtrlBell),
-	.clk_1kHz(SYNTHESIZED_WIRE_2),
-	.clk_500Hz(SYNTHESIZED_WIRE_3),
-	.clk_1Hz(SYNTHESIZED_WIRE_20),
-	.h_cntH(SYNTHESIZED_WIRE_21),
-	.h_cntL(SYNTHESIZED_WIRE_22),
-	.m_cntH(SYNTHESIZED_WIRE_23),
-	.m_cntL(SYNTHESIZED_WIRE_24),
-	.s_cntH(SYNTHESIZED_WIRE_25),
-	.s_cntL(SYNTHESIZED_WIRE_26),
-	.alarm_clock(alarm_beep),
-	.set_hr(SYNTHESIZED_WIRE_18),
-	.set_min(SYNTHESIZED_WIRE_19));
+radio_part	b2v_inst2(
+	
+	
+	.m_cntH(SYNTHESIZED_WIRE_33),
+	.m_cntL(SYNTHESIZED_WIRE_34),
+	.s_cntH(SYNTHESIZED_WIRE_35),
+	.s_cntL(SYNTHESIZED_WIRE_36),
+	.alarm(radio));
 
 
 scan_dig	b2v_inst3(
 	.clk(clk),
-	.rstn(rstn),
+	.rstn(SYNTHESIZED_WIRE_31),
 	.enable(SYNTHESIZED_WIRE_11),
 	.mode(mode),
-	.h_cntH(SYNTHESIZED_WIRE_21),
-	.h_cntL(SYNTHESIZED_WIRE_22),
-	.m_cntH(SYNTHESIZED_WIRE_23),
-	.m_cntL(SYNTHESIZED_WIRE_24),
-	.s_cntH(SYNTHESIZED_WIRE_25),
-	.s_cntL(SYNTHESIZED_WIRE_26),
+	.h_cntH(SYNTHESIZED_WIRE_37),
+	.h_cntL(SYNTHESIZED_WIRE_38),
+	.m_cntH(SYNTHESIZED_WIRE_33),
+	.m_cntL(SYNTHESIZED_WIRE_34),
+	.s_cntH(SYNTHESIZED_WIRE_35),
+	.s_cntL(SYNTHESIZED_WIRE_36),
 	.set_h(SYNTHESIZED_WIRE_18),
 	.set_m(SYNTHESIZED_WIRE_19),
 	.dig(dig),
 	.seg(seg));
 
+assign	SYNTHESIZED_WIRE_31 =  ~rstn;
+
+
+bell_part	b2v_inst7(
+	.setHrKey(setHrKey),
+	.setMinKey(setMinKey),
+	.CtrlBell(CtrlBell),
+	.clk_1kHz(SYNTHESIZED_WIRE_20),
+	.clk_500Hz(SYNTHESIZED_WIRE_21),
+	.clk_1Hz(SYNTHESIZED_WIRE_32),
+	.h_cntH(SYNTHESIZED_WIRE_37),
+	.h_cntL(SYNTHESIZED_WIRE_38),
+	.m_cntH(SYNTHESIZED_WIRE_33),
+	.m_cntL(SYNTHESIZED_WIRE_34),
+	.s_cntH(SYNTHESIZED_WIRE_35),
+	.s_cntL(SYNTHESIZED_WIRE_36),
+	.alarm_clock(alarm),
+	.set_hr(SYNTHESIZED_WIRE_18),
+	.set_min(SYNTHESIZED_WIRE_19));
+
 
 clk_en_generator	b2v_inst8(
 	.clk(clk),
-	.rstn(rstn),
-	.enable(SYNTHESIZED_WIRE_3));
-	defparam	b2v_inst8.N = 10;
+	.rstn(SYNTHESIZED_WIRE_31),
+	.enable(SYNTHESIZED_WIRE_21));
+	defparam	b2v_inst8.N = 100000;
 	defparam	b2v_inst8.W = 17;
 
 
 clk_en_generator	b2v_inst9(
 	.clk(clk),
-	.rstn(rstn),
-	.enable(SYNTHESIZED_WIRE_20));
-	defparam	b2v_inst9.N = 5000;
+	.rstn(SYNTHESIZED_WIRE_31),
+	.enable(SYNTHESIZED_WIRE_32));
+	defparam	b2v_inst9.N = 50000000;
 	defparam	b2v_inst9.W = 26;
 
 
